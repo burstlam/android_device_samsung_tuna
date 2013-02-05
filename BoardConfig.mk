9a46716702a0182efd3b5217859771398eff627e
burstlam <burstlam@gmail.com>
2013-02-05 22:29:22 +0800

    fno-strict for bluetooth
    Change-Id: I2b77c507c4a8e80f497942f4395e500e69fd6f5e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -84,6 +84,7 @@ WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcmdhd_apsta.bin"
 
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_HAVE_BLUETOOTH_NO_STRICT := true
 
 # Boot animation
 TARGET_BOOTANIMATION_PRELOAD := true
